03fb85af06a100692842bf030c1708db09223004
Melissa Aime <aimemelissa1@gmail.com>
2022-06-21 17:06:15 -0300

    navbar and contact

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route , Navigate } from 'react-router-dom';
+import { NavBar } from "./Components/Navbar";
+import { Contact } from "./Components/Contact";
 
 function App() {
-	return <div className='appContainer'>START</div>;
+	return (
+	// <div className='appContainer'>START</div>	
+		<BrowserRouter> 
+  
+            <NavBar/>
+  
+            <Routes>
+                <Route path='/contact' element={ <Contact/> } />
+                <Route path='*' element={ <Navigate to='/' /> } />
+            </Routes>
+  
+        </BrowserRouter>
+	)
+	
+
+		
+
 }
 
 export default App;
