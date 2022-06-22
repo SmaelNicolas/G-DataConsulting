2ea9d0fe2f00bf59390c99f7205116a47cc6ec42
Smael Nicolas <nico.smael@gmail.com>
2022-06-22 19:40:08 -0300

    add company section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route , Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { NavBar } from "./Components/Navbar";
 import { Contact } from "./Components/Contact";
+import { TheCompany } from "./Components/TheCompany/TheCompany";
 
 function App() {
 	return (
-	// <div className='appContainer'>START</div>	
-		<BrowserRouter> 
-  
-            <NavBar/>
-  
-            <Routes>
-                <Route path='/contact' element={ <Contact/> } />
-                <Route path='*' element={ <Navigate to='/' /> } />
-            </Routes>
-  
-        </BrowserRouter>
-	)
-	
+		// <div className='appContainer'>START</div>
+		<BrowserRouter>
+			<NavBar />
 
-		
+			<Routes>
+				<Route path='/contact' element={<Contact />} />
+				{/* <Route path='*' element={<Navigate to='/' />} /> */}
+			</Routes>
 
+			<TheCompany />
+		</BrowserRouter>
+	);
 }
 
 export default App;
