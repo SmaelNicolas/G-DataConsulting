d450ff09bce8d0bfdcf76b0b26a986ac4396c474
Smael Nicolas <nico.smael@gmail.com>
2022-06-22 17:21:33 -0300

    add language context provider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import "./Assets/Fonts/SegoeSmallLight.ttf";
 
 import "./index.css";
 import App from "./App";
+import LanguageContextProvider from "./Context/LanguageContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<LanguageContextProvider>
+			<App />
+		</LanguageContextProvider>
 	</React.StrictMode>
 );
