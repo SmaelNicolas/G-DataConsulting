6c43b9297cc5ec999a0f0af5e8c0afa89e29bb8f
Smael Nicolas <nico.smael@gmail.com>
2022-06-23 20:10:48 -0300

    service section

---
+++ src/Sections/OurServices/OurServices.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { CardServices } from "../../Components/CardServices/CardServices";
+import { Title } from "../../Components/Title/Title";
+
+import "./ourServices.css";
+
+export const OurServices = () => {
+	return (
+		<div className='sectionContainer'>
+			<Title text={"nuestros servicios"} />
+			<div className='cardsContainer'>
+				<CardServices />
+				<CardServices />
+				<CardServices />
+			</div>
+		</div>
+	);
+};
