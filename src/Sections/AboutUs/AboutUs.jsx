4d516ae40b5ce16d66cdcb0d260276d15697d810
Smael Nicolas <nico.smael@gmail.com>
2022-07-01 15:03:03 -0300

    fetch texts from context

--- src/Sections/AboutUs/AboutUs.jsx
+++ src/Sections/AboutUs/AboutUs.jsx
@@ -1,19 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Title } from "../../Components/Title/Title";
+import { LanguageContext } from "../../Context/LanguageContext";
 
 import "./aboutUs.css";
 
 export const AboutUs = () => {
+	const { data } = useContext(LanguageContext);
 	return (
 		<div className='sectionContainer darkBlue' id='aboutUs'>
-			<Title text={"¿QUIÉNES SOMOS?"} />
-			<div className='aboutUs--textContainer'>
-				Somos un equipo apasionado por los datos y le ofrecemos un
-				servicio profesional de desarrollo de BI a empresas. Deseamos
-				mostrar y dar la posibilidad a todos los usuarios de conocer el
-				potencial de las herramientas que nos brindan las tecnologías de
-				la información
-			</div>
+			<Title text={data.us.title} />
+			<div className='aboutUs--textContainer'>{data.us.content}</div>
 		</div>
 	);
 };
