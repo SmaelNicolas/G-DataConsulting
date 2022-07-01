dbe5d158407b5233c82058b54dd241a21180152b
Smael Nicolas <nico.smael@gmail.com>
2022-07-01 12:30:21 -0300

    add text

--- src/Sections/OurServices/OurServices.jsx
+++ src/Sections/OurServices/OurServices.jsx
@@ -6,13 +6,23 @@ import "./ourServices.css";
 
 export const OurServices = () => {
 	return (
-		<div className='sectionContainer' id='ourServices'>
+		<div className='sectionContainer darkBlue' id='ourServices'>
 			<Title text={"nuestros servicios"} />
 			<div className='cardsContainer'>
 				<CardServices />
 				<CardServices />
 				<CardServices />
 			</div>
+			<p className='ourServices--text'>
+				Nuestros clientes podrán ver sus datos traducidos en información
+				en tiempo real, para facilitar el seguimiento de los principales
+				indicadores del negocio y la toma de decisiones. El proceso es
+				profesional y metodológico para garantizar el cumplimiento
+				dentro de los plazos establecidos y presupuestados. Además,
+				ofrecemos servicio de mantenimiento, soporte y capacitación de
+				uso de herramientas de BI, dando la posibilidad de auto gestión
+				a nuestros clientes.
+			</p>
 		</div>
 	);
 };
