4b8eee8a630c0f54c4cde519e0264c41f41094c9
Smael Nicolas <nico.smael@gmail.com>
2022-07-05 11:55:45 -0300

    add icon to value cards

--- src/Sections/OurValues/OurValues.jsx
+++ src/Sections/OurValues/OurValues.jsx
@@ -3,6 +3,12 @@ import "./ourValues.css";
 import { CardValues } from "../../Components/CardValues/CardValues";
 import { Title } from "../../Components/Title/Title";
 import { LanguageContext } from "../../Context/LanguageContext";
+import { MdCenterFocusWeak } from "react-icons/md";
+import { FaHandsHelping } from "react-icons/fa";
+import { GiExtraTime } from "react-icons/gi";
+import { FaPeopleCarry } from "react-icons/fa";
+import { AiOutlineFileProtect } from "react-icons/ai";
+import { IoIosPeople } from "react-icons/io";
 
 export const OurValues = () => {
 	const { data } = useContext(LanguageContext);
@@ -10,9 +16,24 @@ export const OurValues = () => {
 		<div className='sectionContainer darkBlue' id='ourValues'>
 			<Title text={data.values.title} />
 			<div className='ourValues--card--container'>
-				{data.values.cards.map((item, index) => (
-					<CardValues text={item} key={index} />
-				))}
+				<CardValues
+					iconValue={<MdCenterFocusWeak />}
+					text='Foco en el cliente'
+				/>
+				<CardValues iconValue={<FaHandsHelping />} text='Compromiso' />
+				<CardValues
+					iconValue={<GiExtraTime />}
+					text='Responsabilidad'
+				/>
+				<CardValues iconValue={<FaPeopleCarry />} text='Empatía' />
+				<CardValues
+					iconValue={<AiOutlineFileProtect />}
+					text='Profesionalismo'
+				/>
+				<CardValues
+					iconValue={<IoIosPeople />}
+					text='Trabajo en equipo'
+				/>
 			</div>
 		</div>
 	);
