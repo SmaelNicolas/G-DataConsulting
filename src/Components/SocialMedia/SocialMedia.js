f26f0a6e3dd8c26c1042ceae936fd5b379f72ab4
Melissa Aime <aimemelissa1@gmail.com>
2022-07-08 20:38:26 -0300

    socialmedia fixed on scroll

--- src/Components/SocialMedia/SocialMedia.js
+++ src/Components/SocialMedia/SocialMedia.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import { BsLinkedin, BsWhatsapp } from 'react-icons/bs';
 
 export const SocialMedia = () => {
 
+    const [show, setShow] = useState(false);
+    const showLinks = () =>{
+        if(window.scrollY >= 600){
+            setShow(false);
+        }
+        else{
+            setShow(true);
+        }
+    };
+    window.addEventListener('scroll', showLinks);
+
     return(
-        <div className="links-Fixed">
+        <div className={show ? 'links-hide' : 'links-Fixed'}>
             {/* Linkedin - Link */}
             <a href="https://www.linkedin.com/in/gilligm/" target="_blank" id='link-Fixed-logo'> <BsLinkedin/> </a>
             {/* Whatsapp - Link */}
